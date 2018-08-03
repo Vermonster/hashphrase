8707b2e1d2584aec634e347463662aea7319634e
Brian Kaney <brian@vermonster.com>
2018-08-03 15:23:18 -0400

    WIP

--- App.js
+++ App.js
@@ -1,28 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Container, Content, Text, Footer, Header, Title, Button, Form, Item, Input, Card, CardItem, Body } from 'native-base';
+import { Keyboard, Clipboard } from 'react-native';
 import loplop from 'loplop';
 
 export default class App extends React.Component {
+  state = {
+    length: 16,
+    label: '',
+    password: '',
+    showCopy: false,
+    legacyMode: false
+  }
+
+  generatePassword = (label, password) => {
+    Keyboard.dismiss();
+    Clipboard.setString(loplop(label, password, this.state.length));
+    this.setState({showCopy: true});
+  }
+
+  toggleLegacyMode = (newLegacyMode) => {
+    if (newLegacyMode) {
+      this.setState({legacyMode: true, length: 8});
+    } else {
+      this.setState({legacyMode: false, length: 16});
+    }
+  }
 
   render() {
-    let password = loplop('*amazon', 'password');
+    const showCopy = this.state.showCopy;
+    let copy;
+    if (showCopy) {
+      copy = <Card><CardItem><Body><Text>Generated password was copied to the clipboard</Text></Body></CardItem></Card>
+    }
 
     return (
-      <View style={styles.container}>
-        <Text>Password for amazon and password : {password}</Text>
-        <Text>Open test up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container>
+        <Header>
+          <Body>
+            <Title>Oplop</Title>
+          </Body>
+        </Header>
+        <Content padder>
+          <Form>
+            <Item>
+              <Input placeholder="Label..." autoCapitalize="none" onChangeText={(label) => this.setState({label})}/>
+            </Item>
+            <Item last>
+              <Input placeholder="Master Password..." secureTextEntry={true} autoCapitalize="none" onChangeText={(password) => this.setState({password})}/>
+            </Item>
+          </Form>
+
+          <Button full
+            onPress={() => this.generatePassword(this.state.label, this.state.password)}>
+            <Text>Generate Password</Text>
+          </Button>
+          {copy}
+        </Content>
+        <Footer/>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
