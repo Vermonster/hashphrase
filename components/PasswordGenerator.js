4d35ffbd204dc9b4979c223eee9d136d9c41e434
Vicki Strateman <vicki@vermonster.com>
2018-09-07 10:36:22 -0400

    delete unnecessary code from passwordGenerator

--- components/PasswordGenerator.js
+++ components/PasswordGenerator.js
@@ -9,73 +9,39 @@ export default class PasswordGenerator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      length: 16,
       label: '',
       password: '',
-      legacyMode: false,
-      onChanged: false,
     };
     this.generatePassword = this.generatePassword.bind(this);
-    // this.toggleLegacyMode = this.toggleLegacyMode.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(e) {
-    this.setState({ label: e.target.value });
-    this.setState({ password: e.target.value });
-    // this.setState({ [label]: e.target.value }, () => console.log('test'));
-    // this.setState({ [e.target.name]: e.target.value });
-    // console.log(`value is ${e.target.value}`);
-  }
+    handleChange = name => (e) => {
+      const inputValue = e.nativeEvent.text;
+      this.setState({ [name]: inputValue });
+    };
 
-  generatePassword(e) {
-    e.preventDefault();
-    console.log(`${this.state.label}`);
+    generatePassword = (e) => {
+      e.preventDefault();
+      const { label, password } = this.state;
+      const { onClipboardSave } = this.props;
+      Keyboard.dismiss();
+      Clipboard.setString(loplop(label, password));
+      onClipboardSave();
+    }
 
-    // how to handle toggling to legacy? errors handled?
-    // deal with async... where?
-    // const isLegacy = ((this.state.label).lastIndexOf('*', 0)) > -1;
-    // console.log(`${isLegacy}`);
-    // SET STATE for legacymode and then rest is callback? or on onchange?
-    const {
-      label, password, length,
-    } = this.state;
-    const { onClipboardSave } = this.props;
-    Keyboard.dismiss();
-    Clipboard.setString(loplop(label, password, length));
-    onClipboardSave();
-    // console.log(`${this.state.label.length}`);
-    // console.log(`${this.state.label}`);
-  }
-
-
-
-    // handleChange = (e) => {
-    //   console.log(e);
-    //   this.setState
-    //   // this.setState({ [e.target.name]: e.target.value });
-    //   // console.log(`value is ${e.target.value}`);
-    // };
-
-    // toggleLegacyMode = (newLegacyMode) => {
-    //   if (newLegacyMode) {
-    //     this.setState({ legacyMode: true, length: 8 });
-    //   } else {
-    //     this.setState({ legacyMode: false, length: 16 });
-    //   }
-    // }
-
-  render() {
-    return (
-      <Form>
-        <Item>
-          <FormInput name="label" value={this.state.label} placeholder="Label..." onChanged={this.handleChange} />
-        </Item>
-        <Item last>
-          <FormInput name="password" value={this.state.password} placeholder="Master Password..." secureTextEntry onChanged={this.handleChange} />
-        </Item>
-        <BaseButton onPress={this.generatePassword} buttonText="Generate Password" />
-      </Form>
-    );
-  }
+    render() {
+      const { label, password } = this.state;
+      return (
+        <Form>
+          <Item>
+            <FormInput name="label" value={label} placeholder="Label..." onChanged={this.handleChange('label')} />
+          </Item>
+          <Item last>
+            <FormInput name="password" value={password} placeholder="Master Password..." secureTextEntry onChanged={this.handleChange('password')} />
+          </Item>
+          <BaseButton onPress={this.generatePassword} buttonText="Generate Password" />
+        </Form>
+      );
+    }
 }
