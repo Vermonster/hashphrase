36c636353b0b8fa594c7a80afe3c8ace92e11421
Vicki Strateman <vicki@vermonster.com>
2018-10-01 15:05:25 -0400

    fix from pull request review

--- __tests__/InfoScreen-test.js
+++ __tests__/InfoScreen-test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { View } from 'react-native';
-import { Card, Text } from 'react-native-paper';
 import InfoScreen from '../src/navigation/screens/InfoScreen';
 
 // currently a placeholder component for future expert tips/info screen
@@ -9,13 +7,5 @@ describe('<InfoScreen />', () => {
   it('should render the same snapshot', () => {
     const wrapper = shallow(<InfoScreen />);
     expect(wrapper.dive()).toMatchSnapshot();
-  });
-
-  it('should render the the correct components', () => {
-    const requiredComponents = [View, Text, Card];
-    const wrapper = shallow(<InfoScreen />);
-    requiredComponents.forEach((component) => {
-      expect(wrapper.find(component)).toHaveLength(1);
-    });
   });
 });
