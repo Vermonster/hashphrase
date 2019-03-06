b83cfc9e381ced98f4f70dd9f45994f36ab9f530
Vicki Strateman <vicki@vermonster.com>
2019-03-06 13:04:34 -0500

    add placeholder file for new password route

---
+++ src/navigation/screens/CreateNewPassword.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Appbar } from 'react-native-paper';
+import { View } from 'react-native';
+import PasswordGenerator from '../../../components/PasswordGenerator';
+import BaseNotification from '../../../components/BaseNotification';
+
+class CreateNewPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCopy: false,
+    };
+
+    this.showNotification = this.showNotification.bind(this);
+  }
+
+  showNotification = () => {
+    this.setState({ showCopy: true });
+  }
+
+  render() {
+    const copy = 'Generated password was copied to the clipboard';
+    const { showCopy } = this.state;
+
+    return (
+      <View>
+        <Appbar.Header>
+          <Appbar.Content title="Placeholder file for a future component" />
+        </Appbar.Header>
+        <PasswordGenerator onClipboardSave={this.showNotification} />
+        { showCopy && <BaseNotification message={copy} /> }
+      </View>
+    );
+  }
+}
+
+export default CreateNewPassword;
