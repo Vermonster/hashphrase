089a179d905ed8fa62cef734f6b8a2ce33e7593e
Vicki Strateman <vicki@vermonster.com>
2019-03-06 13:03:37 -0500

    add route for new password flow

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import CreateOldPassword from './src/navigation/screens/CreateOldPassword';
+import CreateNewPassword from './src/navigation/screens/CreateNewPassword';
 import FlowScreen from './src/navigation/screens/FlowScreen';
 import InfoScreen from './src/navigation/screens/InfoScreen';
 
 const RootStack = createStackNavigator({
   OldPassword: { screen: CreateOldPassword },
+  NewPassword: { screen: CreateNewPassword },
   Info: { screen: InfoScreen },
   FlowScreen: { screen: FlowScreen },
 },
