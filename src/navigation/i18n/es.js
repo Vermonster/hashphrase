c1e6affc447a7ec150b82e7fd8e04f92ccdddfa7
Vicki Strateman <vicki@vermonster.com>
2019-03-07 15:16:06 -0500

    edit title

--- src/navigation/i18n/es.js
+++ src/navigation/i18n/es.js
@@ -1,6 +1,6 @@
 const es = {
   createPassword: {
-    title: 'Agilikey',
+    title: 'AgiliKEY',
     message: 'Contraseña generada ha sido copiado al clipboard.',
     label: 'Etiqueta',
     password: 'Contraseña general',
