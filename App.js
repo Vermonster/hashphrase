e3388b55eb55bd16995694b448509a8fe4962f84
Vicki Strateman <vicki@vermonster.com>
2019-03-07 14:33:11 -0500

    delete createNewPassword components because navigation will provide different states, not different components

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+<<<<<<< HEAD
 import CreatePassword from './src/navigation/screens/CreatePassword';
+=======
+import CreateOldPassword from './src/navigation/screens/CreateOldPassword';
+import FlowScreen from './src/navigation/screens/FlowScreen';
+>>>>>>> delete createNewPassword components because navigation will provide different states, not different components
 import InfoScreen from './src/navigation/screens/InfoScreen';
 import FlowScreen from './src/navigation/screens/FlowScreen';
 
 const RootStack = createStackNavigator({
+<<<<<<< HEAD
   PasswordScreen: { screen: CreatePassword },
   InfoScreen: { screen: InfoScreen },
+=======
+  OldPassword: { screen: CreateOldPassword },
+  NewPassword: { screen: CreateOldPassword },
+  Info: { screen: InfoScreen },
+>>>>>>> delete createNewPassword components because navigation will provide different states, not different components
   FlowScreen: { screen: FlowScreen },
 },
 {
