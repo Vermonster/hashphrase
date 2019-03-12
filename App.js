0c4386cc0cc7e38407d83890d310b40cf0d7a4ed
Vicki Strateman <vicki@vermonster.com>
2019-03-12 16:15:54 -0400

    delete extra files from rebase

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+<<<<<<< HEAD
 import { I18nextProvider } from 'react-i18next';
 import i18n from './src/navigation/i18n/i18n';
+=======
+>>>>>>> delete extra files from rebase
 import CreatePassword from './src/navigation/screens/CreatePassword';
 import InfoScreen from './src/navigation/screens/InfoScreen';
 
+<<<<<<< HEAD
 const AppStackNavigator = createStackNavigator({
   PasswordScreen: { screen: CreatePassword },
   InfoScreen: { screen: InfoScreen },
   initialRouteName: 'PasswordScreen',
+=======
+const RootStack = createStackNavigator({
+  PasswordScreen: { screen: CreatePassword },
+  InfoScreen: { screen: InfoScreen },
+  FlowScreen: { screen: FlowScreen },
+},
+{
+  initialRouteName: 'FlowScreen',
+>>>>>>> delete extra files from rebase
 });
 
 const AppContainer = createAppContainer(AppStackNavigator);
