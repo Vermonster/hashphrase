1cd3b0a2433cdbb696889374b31d70e8bc2a48e5
Olivia Zhang <olivia@vermonster.com>
2019-03-12 14:21:50 -0400

    fix typo

--- __tests__/CreatePassword-test.js
+++ __tests__/CreatePassword-test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CreatePassword from '../src/navigation/screens/CreatePassword';
 
-describe('/CreatePassword />', () => {
+describe('CreatePassword />', () => {
   const navigation = { getParam: jest.fn() };
   const wrapper = shallow(<CreatePassword navigation={navigation} />);
 
