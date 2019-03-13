41d2bc514ec1813594b61de784f7030a5b583c3b
Olivia Zhang <olivia@vermonster.com>
2019-03-13 11:31:11 -0400

    dim background when modal is launched

--- src/components/ConfirmationDialog.js
+++ src/components/ConfirmationDialog.js
@@ -5,66 +5,68 @@ import {
 import { Button, Checkbox } from 'react-native-paper';
 
 const styles = StyleSheet.create({
-  contentWrap: {
-    marginTop: 150,
-    borderWidth: 1,
-    justifyContent: 'center',
+  container: {
+    flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  modalBackground: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  innerContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
   },
 });
 
 export default class ConfirmationDialog extends React.Component {
   state = { checked: false }
 
-  clearClipboard = () => {
-
-  }
-
-  handleSubmit = () => {
-
-  }
-
   render() {
     const { visible, generatedPassword } = this.props;
     const { checked } = this.state;
-    console.log(checked);
+
     return (
-      <Modal
-        animationType="slide"
-        transparent={false}
-        visible={visible}
-        onRequestClose={() => { }}
-        style={styles.modal}
-      >
-        <View style={styles.contentWrap}>
-          <View>
-            <Text>All done!</Text>
-            <Text>Your account password is now on your clipboard.</Text>
-          </View>
-          <View>
-            <Text>
-              Account Password:
-              <Text>{generatedPassword}</Text>
-            </Text>
-          </View>
-          <View>
-            <Text>What would you like to do next?</Text>
-            <View>
-              <Text>Clear clipboard</Text>
-              <Checkbox status="checked" onPress={() => this.setState({ checked: !checked })} />
+      <View style={styles.container}>
+        <Modal
+          animationType="fade"
+          transparent
+          visible={visible}
+          onRequestClose={() => { }}
+        >
+          <View style={[styles.container, styles.modalBackground]}>
+            <View style={styles.innerContainer}>
+              <View>
+                <Text>All done!</Text>
+                <Text>Your account password is now on your clipboard.</Text>
+              </View>
+              <View>
+                <Text>
+                  Account Password:
+                  <Text>{generatedPassword}</Text>
+                </Text>
+              </View>
+              <View>
+                <Text>What would you like to do next?</Text>
+                <View>
+                  <Text>Clear clipboard</Text>
+                  <Checkbox status="checked" onPress={() => this.setState({ checked: !checked })} />
+                </View>
+              </View>
+              <Button
+                onPress={() => {}}
+                accessibilityLabel="CHOOSE ACTION BUTTON"
+                mode="contained"
+                dark
+                color="#D37F26"
+              >
+                OKAY
+              </Button>
             </View>
           </View>
-        </View>
-        <Button
-          onPress={this.handleSubmit}
-          accessibilityLabel="CHOOSE ACTION BUTTON"
-          mode="contained"
-          dark
-          color="#D37F26"
-        >
-          OKAY
-        </Button>
-      </Modal>
+        </Modal>
+      </View>
     );
   }
 }
