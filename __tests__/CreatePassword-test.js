3f8626d077ab49ab238fd11eb293b3aab765fb71
Olivia Zhang <olivia@vermonster.com>
2019-03-13 13:15:35 -0400

    add some basic snapshot tests and unit tests

--- __tests__/CreatePassword-test.js
+++ __tests__/CreatePassword-test.js
@@ -4,9 +4,14 @@ import CreatePassword from '../src/navigation/screens/CreatePassword';
 
 describe('<CreatePassword />', () => {
   const navigation = { getParam: jest.fn() };
-  const wrapper = shallow(<CreatePassword navigation={navigation} />);
+  const wrapper = shallow(<CreatePassword navigation={navigation} isNewPassword={false} />);
 
   it('should render the same snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update the component state when handleToggleSwitch is called', () => {
+    wrapper.instance().handleToggleSwitch();
+    expect(wrapper.state('isNewPassword')).toBe(true);
   });
 });
