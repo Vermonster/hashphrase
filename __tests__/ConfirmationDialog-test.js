a647f68f4e05f214e0c521aeb939a86bddddb47a
Olivia Zhang <zhangyi.olivia@gmail.com>
2019-03-14 13:13:16 -0400

    add unit test

--- __tests__/ConfirmationDialog-test.js
+++ __tests__/ConfirmationDialog-test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Checkbox } from 'react-native-paper';
 import ConfirmationDialog from '../src/components/ConfirmationDialog';
 
 describe('<ConfirmationDialog />', () => {
-  const wrapper = shallow(<ConfirmationDialog />);
+  const wrapper = shallow(<ConfirmationDialog clearClipboard={false} />);
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update the state when clear checkbox is clicked', () => {
+    wrapper.find(Checkbox).simulate('press');
+    expect(wrapper.state('clearClipboard')).toBe(true);
   });
 });
