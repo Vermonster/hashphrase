c6d52a85c1442ad960f0428e9ffba7c8cb0292b2
Vicki Strateman <vicki@vermonster.com>
2019-03-15 10:37:34 -0400

    add english and spanish text

--- src/navigation/i18n/en.js
+++ src/navigation/i18n/en.js
@@ -5,6 +5,13 @@ const en = {
     label: 'Nickname',
     password: 'Master Password',
     button: 'Create account password',
+    newPassword: 'Creating a new account password?',
+    passwordLabel: 'What is the account password\'s Nickname?',
+    masterPassword: 'What is your Master Password?',
+    confirmPassword: 'Retype your Master Password',
+    submitMessage: 'CREATE ACCOUNT PASSWORD',
+    oldPasswordFlow: 'Old Password Flow',
+    newPasswordFlow: 'New Password Flow',
   },
 };
 
--- src/navigation/i18n/es.js
+++ src/navigation/i18n/es.js
@@ -1,10 +1,17 @@
 const es = {
-  createPassword: {
+  translation: {
     title: 'AgiliKEY',
     message: 'Contraseña generada ha sido copiado al clipboard.',
     label: 'Etiqueta',
     password: 'Contraseña general',
     button: 'Cree una contraseña',
+    newPassword: '¿Vas a crear una nueva contraseña?',
+    passwordLabel: '¿Qué es la etiqueta de la cuenta?',
+    masterPassword: '¿Qué es la Contraseña Maestra?',
+    confirmPassword: 'Vuelva a escribir la Contraseña Maestra',
+    submitMessage: 'GENERE CONTRASEÑA',
+    oldPasswordFlow: 'Contraseña Vieja',
+    newPasswordFlow: 'Contraseña Nueva',
   },
 };
 
