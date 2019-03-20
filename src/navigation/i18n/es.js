70be1168c5bd075eb2d66527ca0ba2aa5c53b411
Vicki Strateman <vicki@vermonster.com>
2019-03-20 16:00:13 -0400

    remove unused i18n text

--- src/navigation/i18n/es.js
+++ src/navigation/i18n/es.js
@@ -2,23 +2,7 @@ const es = {
 
   createPassword: {
     title: 'AgiliKEY',
-    label: 'Etiqueta',
-    password: 'Contraseña general',
-    button: 'Crear una contraseña',
     newPassword: '¿Vas a crear una nueva contraseña?',
-    passwordLabel: '¿Qué es la etiqueta de la cuenta?',
-    masterPassword: '¿Qué es la Contraseña Maestra?',
-    confirmPassword: 'Vuelva a escribir la Contraseña Maestra',
-    submitMessage: 'GENERAR CONTRASEÑA',
-    oldPasswordFlow: 'Contraseña Vieja',
-    newPasswordFlow: 'Contraseña Nueva',
-    completedStatus: '¡Completo!',
-    completedClipboard: 'Contraseña generada ha sido copiado al clipboard.',
-    accountPassword: 'Contraseña de la Cuenta:',
-    nextSteps: '¿Qué le gusta hacer ahora?',
-    clearClipboard: 'Borrar el clipboard',
-    anotherPassword: 'Hacer otra contraseña',
-    confirmOk: 'CONFIRMAR',
   },
   confirmationDialog: {
     completedStatus: '¡Completo!',
