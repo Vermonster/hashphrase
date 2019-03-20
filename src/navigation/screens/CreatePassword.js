01b48962a5c6c2e07aef844ac02fe2be4fef50fb
Olivia Zhang <olivia@vermonster.com>
2019-03-20 16:17:16 -0400

    add basic styles using base style objects

--- src/navigation/screens/CreatePassword.js
+++ src/navigation/screens/CreatePassword.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import { Switch, Appbar } from 'react-native-paper';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { withNamespaces } from 'react-i18next';
+import Logo from '../../styles/icons';
 import PasswordGenerator from '../../components/PasswordGenerator';
+import { colors, rowCenter, fontSize } from '../../styles/base';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  header: {
+    backgroundColor: colors.white,
+  },
+  newPass: {
+    ...rowCenter,
+    marginVertical: 32,
+
+  },
+  logo: {
+    ...StyleSheet.absoluteFill,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  newPassLabel: {
+    fontSize: fontSize.lg,
+  },
+});
 
 class CreateNewPassword extends Component {
   constructor(props) {
@@ -21,15 +46,20 @@ class CreateNewPassword extends Component {
     const { t } = this.props;
 
     return (
-      <View>
-        <Appbar.Header>
-          <Appbar.Content title={t('title')} />
+      <View style={styles.container}>
+        <Appbar.Header style={styles.header}>
+          <View style={styles.logo}>
+            <Logo />
+          </View>
+          <Appbar.Content title="AgiliKEY" />
+          <Appbar.Action icon="info" color={colors.primary} size={28} onPress={() => { }} />
         </Appbar.Header>
-        <View>
-          <Text>{t('newPassword')}</Text>
+        <View style={styles.newPass}>
+          <Text style={styles.newPassLabel}>{t('newPassword')}</Text>
           <Switch
             value={isNewPassword}
             onValueChange={this.handleToggleSwitch}
+            color={colors.primary}
           />
         </View>
         <PasswordGenerator
