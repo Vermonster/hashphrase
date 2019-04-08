c07111d2d1f382e39501476834a4c36001f6a551
Vicki Strateman <vicki@vermonster.com>
2019-04-08 12:29:51 -0400

    add darkgray color to base styles for tips page text

--- src/styles/base.js
+++ src/styles/base.js
@@ -4,6 +4,7 @@ export const colors = {
   warning: '#DB4437',
   disabled: '#949494',
   background: '#F5F5F5',
+  darkGray: '#444545',
   white: '#FFF',
   modalBackground: 'rgba(0, 0, 0, 0.5)',
 };
