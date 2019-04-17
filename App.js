ceb808d2cc31873ed63348d7a025d324a0033d21
Vicki Strateman <vicki@vermonster.com>
2019-04-17 12:00:44 -0400

    place safeAreaView modification at top of app

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator, SafeAreaView } from 'react-navigation';
 import { I18nextProvider } from 'react-i18next';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import i18n from './src/navigation/i18n/i18n';
 import CreatePassword from './src/navigation/screens/CreatePassword';
 import TipsPage from './src/navigation/screens/TipsPage';
 import { colors } from './src/styles/base';
+
+if (process.env.NODE_ENV !== 'test') {
+  SafeAreaView.setStatusBarHeight(0);
+}
 
 const theme = {
   ...DefaultTheme,
