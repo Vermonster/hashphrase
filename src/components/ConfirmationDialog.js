757510db2665da1fbcd3470c66a213677dab2074
Olivia Zhang <olivia@vermonster.com>
2019-04-18 09:52:29 -0400

    add clearForm to the close modal button

--- src/components/ConfirmationDialog.js
+++ src/components/ConfirmationDialog.js
@@ -105,7 +105,7 @@ class ConfirmationDialog extends React.Component {
   render() {
     const { obscured, height } = this.state;
     const {
-      t, visible, generatedPassword, closeModal, clearInputs,
+      t, visible, generatedPassword, closeModal, clearForm,
     } = this.props;
     const visibilityIcon = obscured ? 'visibility-off' : 'visibility';
     const accountLabelMargin = Platform.OS === 'ios' ? (0.02 * height) : 0;
@@ -129,7 +129,7 @@ class ConfirmationDialog extends React.Component {
                   icon="close"
                   size={35}
                   style={styles.closeButton}
-                  onPress={() => { closeModal(); clearInputs(); }}
+                  onPress={() => { closeModal(); clearForm(); }}
                 />
                 <View style={{ marginLeft: '4%' }}>
                   <Text style={styles.title}>{t('completedStatus')}</Text>
