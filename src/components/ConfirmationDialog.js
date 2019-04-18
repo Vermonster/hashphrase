2c313ba8f0f8ca2197bfefb89a886ec9dfc28b24
Vicki Strateman <vicki@vermonster.com>
2019-04-18 14:55:13 -0400

    remove messages continer flex

--- src/components/ConfirmationDialog.js
+++ src/components/ConfirmationDialog.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     borderRadius: 4,
   },
   messagesContainer: {
-    // flex: 1 / 3,
+    flex: 2 / 3,
     backgroundColor: colors.primary,
     borderTopRightRadius: 4,
     borderTopLeftRadius: 4,
@@ -120,13 +120,10 @@ class ConfirmationDialog extends React.Component {
 
     if (height > 800) {
       styling.innerContainerFlex = 1 / 2;
-      styling.messagesContainerFlex = 2 / 3;
     } else if (height < 376) {
       styling.innerContainerFlex = 4 / 5;
-      styling.messagesContainerFlex = 2 / 3;
     } else {
       styling.innerContainerFlex = 2 / 3;
-      styling.messagesContainerFlex = 2 / 3;
     }
 
     return styling;
@@ -142,7 +139,7 @@ class ConfirmationDialog extends React.Component {
     const visibilityIcon = obscured ? 'visibility-off' : 'visibility';
     const accountLabelMargin = Platform.OS === 'ios' ? 6 : 0;
     const dynamicStyles = this.modalStyling(orientation, height, width);
-    const { innerContainerWidth, innerContainerFlex, passwordContainerWidth, messagesContainerFlex } = dynamicStyles;
+    const { innerContainerWidth, innerContainerFlex, passwordContainerWidth } = dynamicStyles;
     console.log(height, width);
 
     return (
@@ -158,7 +155,7 @@ class ConfirmationDialog extends React.Component {
         >
           <View style={[styles.container, styles.modalBackground]}>
             <View style={[styles.innerContainer, { width: innerContainerWidth, flex: innerContainerFlex }]}>
-              <View style={[styles.messagesContainer, { flex: messagesContainerFlex }]}>
+              <View style={styles.messagesContainer}>
                 <IconButton
                   icon="close"
                   size={35}
