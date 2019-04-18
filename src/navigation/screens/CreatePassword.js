9c1d02c4f6dfcde22f504b534f3b8cf3ecc1d3cf
Olivia Zhang <olivia@vermonster.com>
2019-04-18 09:20:41 -0400

    update tests

--- src/navigation/screens/CreatePassword.js
+++ src/navigation/screens/CreatePassword.js
@@ -59,82 +59,82 @@ class CreateNewPassword extends Component {
     ),
   });
 
-state = {
-  isNewPassword: false,
-  snackbarVisibility: false,
-  height: Dimensions.get('window').height,
-};
+  state = {
+    isNewPassword: false,
+    snackbarVisibility: false,
+    height: Dimensions.get('window').height,
+  };
 
-onLayout = () => {
-  const { height } = Dimensions.get('screen');
-  this.setState({ height });
-}
+  onLayout = () => {
+    const { height } = Dimensions.get('screen');
+    this.setState({ height });
+  }
 
-handleToggleSwitch = () => this.setState(({ isNewPassword }) => ({
-  isNewPassword: !isNewPassword,
-}))
+  handleToggleSwitch = () => this.setState(({ isNewPassword }) => ({
+    isNewPassword: !isNewPassword,
+  }))
 
-showSnackbar = () => this.setState({ snackbarVisibility: true });
+  showSnackbar = () => this.setState({ snackbarVisibility: true });
 
-hideSnackbar = () => this.setState({ snackbarVisibility: false });
+  hideSnackbar = () => this.setState({ snackbarVisibility: false });
 
-render() {
-  const { t } = this.props;
-  const {
-    isNewPassword,
-    snackbarVisibility,
-    height,
-  } = this.state;
+  render() {
+    const { t } = this.props;
+    const {
+      isNewPassword,
+      snackbarVisibility,
+      height,
+    } = this.state;
 
-  const marginTopValue = Math.floor(height * (0.15));
-  const marginBottomValue = Math.floor(height * (0.1));
+    const marginTopValue = Math.floor(height * (0.15));
+    const marginBottomValue = Math.floor(height * (0.1));
 
-  return (
-    <SafeAreaView
-      style={styles.container}
-      onLayout={this.onLayout}
-    >
-      <StatusBar barStyle="light-content" />
-      <KeyboardAvoidingView
-        behavior="padding"
-        keyboardVerticalOffset={100}
-        style={styles.flexLayout}
+    return (
+      <SafeAreaView
+        style={styles.container}
+        onLayout={this.onLayout}
       >
-        <ScrollView style={styles.flexLayout} keyboardShouldPersistTaps="always">
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.innerContainer}>
-              <View
-                style={[styles.newPass,
-                  { marginTop: marginTopValue, marginBottom: marginBottomValue }]
-              }
-              >
-                <Text style={styles.newPassLabel}>{t('newPassword')}</Text>
-                <Switch
-                  value={isNewPassword}
-                  onValueChange={this.handleToggleSwitch}
-                  color={colors.primary}
+        <StatusBar barStyle="light-content" />
+        <KeyboardAvoidingView
+          behavior="padding"
+          keyboardVerticalOffset={100}
+          style={styles.flexLayout}
+        >
+          <ScrollView style={styles.flexLayout} keyboardShouldPersistTaps="always">
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <View style={styles.innerContainer}>
+                <View
+                  style={[styles.newPass,
+                    { marginTop: marginTopValue, marginBottom: marginBottomValue }]
+                  }
+                >
+                  <Text style={styles.newPassLabel}>{t('newPassword')}</Text>
+                  <Switch
+                    value={isNewPassword}
+                    onValueChange={this.handleToggleSwitch}
+                    color={colors.primary}
+                  />
+                </View>
+                <PasswordGenerator
+                  isNewPassword={isNewPassword}
+                  showSnackbar={this.showSnackbar}
                 />
+                <View style={styles.flexLayout} />
               </View>
-              <PasswordGenerator
-                isNewPassword={isNewPassword}
-                showSnackbar={this.showSnackbar}
-              />
-              <View style={styles.flexLayout} />
-            </View>
-          </TouchableWithoutFeedback>
-        </ScrollView>
-      </KeyboardAvoidingView>
-      <Snackbar
-        visible={snackbarVisibility}
-        onDismiss={this.hideSnackbar}
-        duration={2000}
-        style={styles.snackbar}
-      >
-        {t('clipboardCleared')}
-      </Snackbar>
-    </SafeAreaView>
-  );
-}
+            </TouchableWithoutFeedback>
+          </ScrollView>
+        </KeyboardAvoidingView>
+        <Snackbar
+          visible={snackbarVisibility}
+          onDismiss={this.hideSnackbar}
+          duration={2000}
+          style={styles.snackbar}
+        >
+          {t('clipboardCleared')}
+        </Snackbar>
+      </SafeAreaView>
+    );
+  }
 }
 
 export default withNamespaces('createPassword')(CreateNewPassword);
