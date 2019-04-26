ac729ea8aba89994279351d5779490a1e2aee3c3
Vicki Strateman <vicki@vermonster.com>
2019-04-26 14:01:02 -0400

    convert formInput to functional component

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -25,8 +25,8 @@ const styles = StyleSheet.create({
   },
 });
 
-class FormInput extends React.Component {
-  isTextHidden = (inputLabel, visibleStatus) => {
+const FormInput = (props) => {
+  const isTextHidden = (inputLabel, visibleStatus) => {
     if (inputLabel === 'label') {
       return false;
     } if (inputLabel === 'password' || inputLabel === 'confirmPassword') {
@@ -35,63 +35,61 @@ class FormInput extends React.Component {
       }
     }
     return true;
-  }
+  };
 
-  render() {
-    const {
-      placeholderText,
-      prompt,
-      value,
-      handleChange,
-      label,
-      componentType,
-      error,
-      handleInputFocus,
-      inputRef,
-      handlePasswordVisibility,
-      visibility,
-    } = this.props;
-    const visibilityIcon = visibility ? 'visibility' : 'visibility-off';
-    const visibleText = this.isTextHidden(componentType, visibility);
-    const buttonType = (componentType === 'password')
-      ? (
-        <IconButton
-          icon={visibilityIcon}
-          onPress={handlePasswordVisibility}
-          accessibilityLabel="hide or show password"
-          style={styles.visibilityButton}
-          color={colors.secondary}
-          testID="hideShowButton"
+  const {
+    placeholderText,
+    prompt,
+    value,
+    handleChange,
+    label,
+    componentType,
+    error,
+    handleInputFocus,
+    inputRef,
+    handlePasswordVisibility,
+    visibility,
+  } = props;
+  const visibilityIcon = visibility ? 'visibility' : 'visibility-off';
+  const visibleText = isTextHidden(componentType, visibility);
+  const buttonType = (componentType === 'password')
+    ? (
+      <IconButton
+        icon={visibilityIcon}
+        onPress={handlePasswordVisibility}
+        accessibilityLabel="hide or show password"
+        style={styles.visibilityButton}
+        color={colors.secondary}
+        testID="hideShowButton"
+      />
+    ) : null;
+
+  const inputMargin = componentType === 'confirmPassword' ? 0 : 30;
+
+  return (
+    <View>
+      <Text style={styles.label}>{prompt}</Text>
+      <View style={[styles.formInputContainer, { marginBottom: inputMargin }]}>
+        <TextInput
+          value={value}
+          placeholder={placeholderText}
+          secureTextEntry={visibleText}
+          style={styles.textInput}
+          onChange={handleChange(componentType)}
+          autoCapitalize="none"
+          autoComplete="off"
+          autoCorrect={false}
+          label={label}
+          error={error}
+          returnKeyType="next"
+          onSubmitEditing={handleInputFocus}
+          ref={inputRef}
+          disableFullscreenUI
         />
-      ) : null;
-
-    const inputMargin = componentType === 'confirmPassword' ? 0 : 30;
-
-    return (
-      <View>
-        <Text style={styles.label}>{prompt}</Text>
-        <View style={[styles.formInputContainer, { marginBottom: inputMargin }]}>
-          <TextInput
-            value={value}
-            placeholder={placeholderText}
-            secureTextEntry={visibleText}
-            style={styles.textInput}
-            onChange={handleChange(componentType)}
-            autoCapitalize="none"
-            autoComplete="off"
-            autoCorrect={false}
-            label={label}
-            error={error}
-            returnKeyType="next"
-            onSubmitEditing={handleInputFocus}
-            ref={inputRef}
-            disableFullscreenUI
-          />
-          { buttonType }
-        </View>
+        { buttonType }
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 export default FormInput;
