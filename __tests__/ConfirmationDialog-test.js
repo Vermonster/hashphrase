31592f7d53e192a756bf34b499132fd382c72bc0
Vicki Strateman <vicki@vermonster.com>
2019-05-02 14:17:19 -0400

    add tests

--- __tests__/ConfirmationDialog-test.js
+++ __tests__/ConfirmationDialog-test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { IconButton } from 'react-native-paper';
 import { TestConfirmationDialog as ConfirmationDialog } from '../src/components/ConfirmationDialog';
 
 describe('<ConfirmationDialog />', () => {
@@ -20,5 +22,38 @@ describe('<ConfirmationDialog />', () => {
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should change obscured state when hide/show button clicked', () => {
+    const instance = wrapper.instance();
+    expect(instance.state.obscured).toBe(true);
+    wrapper.findWhere(node => node.prop('testID') === 'hide-show-button').simulate('press');
+    expect(instance.state.obscured).toBe(false);
+  });
+
+  it('should reset obscured state when start over button clicked', () => {
+    const instance = wrapper.instance();
+    instance.state.obscured = false;
+    expect(instance.state.obscured).toBe(false);
+    wrapper.findWhere(node => node.prop('testID') === 'start-over-button').simulate('press');
+    expect(instance.state.obscured).toBe(true);
+  });
+
+  it('should render initially with icon visibility-off', () => {
+    const mockFn = jest.fn();
+    const instance = renderer.create(
+      <ConfirmationDialog
+        visible
+        closeModal={mockFn}
+        t={key => key}
+        generatedPassword="12345678"
+        clearForm={mockFn}
+        resetToggleSwitch={mockFn}
+      />,
+    );
+
+    const textInstance = instance.root.findByType(IconButton);
+
+    expect(textInstance.props.icon).toEqual('visibility-off');
   });
 });
