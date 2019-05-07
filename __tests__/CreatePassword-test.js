f8223f4d36e63cdef8d52d3ddd84485ec1db4792
Vicki Strateman <vicki@vermonster.com>
2019-05-07 16:08:10 -0400

    add before() function and write tests for button and state

--- __tests__/CreatePassword-test.js
+++ __tests__/CreatePassword-test.js
@@ -2,23 +2,33 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { TestCreatePassword as CreatePassword } from '../src/navigation/screens/CreatePassword';
 
-// test if pressing touchableOpacity button calls clearclipboard fn
-// 
-// test if clipboard is actually cleared?
+let wrapper;
+let instance;
 
-describe('<CreatePassword />', () => {
-  const wrapper = shallow(
+beforeEach(() => {
+  wrapper = shallow(
     <CreatePassword
       t={key => key}
     />,
   );
+  instance = wrapper.instance();
+});
 
+describe('<CreatePassword />', () => {
   it('should render the same snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should show snackbar when clipboard-clear button is pressed', () => {
-    const instance = wrapper.instance();
+  it('should call clearClipboard() when clipboard-clear button pressed', () => {
+    const spy = jest.spyOn(instance, 'clearClipboard');
+    instance.forceUpdate();
+    wrapper.findWhere(node => node.prop('testID') === 'clipboard-clear').simulate('press');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should update snackbarVisibility state to "true" when clipboard-clear button is pressed', () => {
     expect(instance.state.snackbarVisibility).toBe(false);
+    wrapper.findWhere(node => node.prop('testID') === 'clipboard-clear').simulate('press');
+    expect(instance.state.snackbarVisibility).toBe(true);
   });
 });
