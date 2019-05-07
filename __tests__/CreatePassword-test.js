2ce08b58a0d3489b43654dfc866fcbaef788e0ef
Vicki Strateman <vicki@vermonster.com>
2019-05-07 08:37:09 -0400

    wip createPassword tests

--- __tests__/CreatePassword-test.js
+++ __tests__/CreatePassword-test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { TestCreatePassword as CreatePassword } from '../src/navigation/screens/CreatePassword';
 
+// test if pressing touchableOpacity button calls clearclipboard fn
+// 
+// test if clipboard is actually cleared?
+
 describe('<CreatePassword />', () => {
   const wrapper = shallow(
     <CreatePassword
@@ -11,5 +15,10 @@ describe('<CreatePassword />', () => {
 
   it('should render the same snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should show snackbar when clipboard-clear button is pressed', () => {
+    const instance = wrapper.instance();
+    expect(instance.state.snackbarVisibility).toBe(false);
   });
 });
