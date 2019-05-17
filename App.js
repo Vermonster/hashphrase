402027407253a789eb9fe1f81c1b99945c385d4d
Olivia Zhang <olivia@vermonster.com>
2019-05-17 11:56:03 -0400

    extract theme to its own module

--- App.js
+++ App.js
@@ -2,30 +2,13 @@ import React from 'react';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import { Font } from 'expo';
 import { I18nextProvider } from 'react-i18next';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import i18n from './src/navigation/i18n/i18n';
 import CreatePassword from './src/navigation/screens/CreatePassword';
 import TipsPage from './src/navigation/screens/TipsPage';
 import PolicyPage from './src/navigation/screens/PolicyPage';
 import { colors } from './src/styles/base';
-
-const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#E07926',
-    accent: '#212A59',
-    error: '#DB4437',
-    text: '#444545',
-    placeholder: '#949494',
-    disabled: '#212A59',
-  },
-  fonts: {
-    regular: 'lato-regular',
-    medium: 'lato-bold',
-  },
-};
+import THEME from './src/styles/theme';
 
 const AppStackNavigator = createStackNavigator(
   {
@@ -72,7 +55,7 @@ class App extends React.Component {
     const { fontLoaded } = this.state;
     return (
       <I18nextProvider i18n={i18n}>
-        <PaperProvider theme={theme}>
+        <PaperProvider theme={THEME}>
           { fontLoaded ? <AppContainer /> : null }
         </PaperProvider>
       </I18nextProvider>
